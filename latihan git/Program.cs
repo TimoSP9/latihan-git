ee84ed70cfa262a41ed1acfc97a192e99ab68ad2
USER <USER@LAPTOP-6K190H6B>
2021-12-03 10:48:42 +0700

    Add project files.

---
+++ latihan git/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace latihan_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input prima:");
+            int bilangan = Convert.ToInt32(Console.ReadLine());
+            //ART
+            int ART = 0;
+            for (int i = 1; i <= bilangan; i++)
+            {
+                if (bilangan % i== 0 )
+                {
+                    ART++;
+                }
+            }
+            if (ART == 2)
+            {
+                Console.Write("Ini itu prima");
+            }
+            else
+                Console.Write("ini bukan prima");
+        }
+    }
+}
